ba6d63549a586b7034dea5cbbd2583d37ab368b9
karkiayush <karkiayush619@gmail.com>
2023-08-23 20:21:15 -0700

    fibonacci series finder using network prog is done

--- src/BoardExamPreactise/NetworkProgramming/EchoClient.java
+++ src/BoardExamPreactise/NetworkProgramming/EchoClient.java
@@ -1,13 +1,35 @@
 package BoardExamPreactise.NetworkProgramming;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class EchoClient {
     public static void main(String[] args) {
         System.out.println("Client is started...");
+        Scanner sc = new Scanner(System.in);
         try {
             Socket soc = new Socket("localhost", 9806);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Enter the range of the fibonacci series to be find out: ");
+            String range = br.readLine();
+
+            // true is for auto flush functionality
+            PrintWriter wr = new PrintWriter(soc.getOutputStream(), true);
+            wr.println(range);
+
+            // Reading the fibonacci series
+            BufferedReader fbr = new BufferedReader(new InputStreamReader(System.in));
+            String receiverArray = fbr.readLine();
+            String[] fiboArray = receiverArray.split(", ");
+
+            for (String elements : fiboArray) {
+                int num = Integer.parseInt(elements);
+                System.out.println(num);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/BoardExamPreactise/NetworkProgramming/EchoServer.java
+++ src/BoardExamPreactise/NetworkProgramming/EchoServer.java
@@ -1,5 +1,6 @@
 package BoardExamPreactise.NetworkProgramming;
 
+import java.io.*;
 import java.net.*;
 
 public class EchoServer {
@@ -9,6 +10,28 @@ public class EchoServer {
             ServerSocket ss = new ServerSocket(9806);
             Socket soc = ss.accept();
             System.out.println("Connection is established");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+
+            String userInput = br.readLine();
+            int range = Integer.parseInt(userInput);
+            int fn = 0;
+            int sn = 1;
+            int tn;
+            int[] fiboSeries = new int[range];
+            fiboSeries[0] = fn;
+            fiboSeries[1] = sn;
+
+            for (int i = 2; i <= range; i++) {
+                tn = fn + sn;
+                fiboSeries[i] = tn;
+                fn = sn;
+                sn = tn;
+            }
+
+            // sending the array of fibonacci series to the client
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(fiboSeries);
         } catch (Exception e) {
             e.printStackTrace();
         }
