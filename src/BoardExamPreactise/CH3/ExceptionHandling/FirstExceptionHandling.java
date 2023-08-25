5ab2babeca59e0c33246ac63b91d9204a348dfae
karkiayush <karkiayush619@gmail.com>
2023-08-25 22:20:44 +0545

    custom exception handling is done halfway

---
+++ src/BoardExamPreactise/CH3/ExceptionHandling/FirstExceptionHandling.java
@@ -0,0 +1,16 @@
+
+/*UserDefined Exception*/
+
+package BoardExamPreactise.CH3.ExceptionHandling;
+
+import java.lang.Throwable;
+
+class MyException extends Exception {
+
+}
+
+public class FirstExceptionHandling {
+    public static void main(String[] args) {
+
+    }
+}
