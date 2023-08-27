fce52e84d2756dccfea51b6b6f42b8446c458c73
karkiayush <karkiayush619@gmail.com>
2023-08-27 15:10:10 +0545

    Nepal flag is made

---
+++ src/BoardExamPreactise/CH6/NinteenSpFourA.java
@@ -0,0 +1,53 @@
+package BoardExamPreactise.CH6;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NinteenSpFourA extends JFrame {
+    private JTextField radiusTextField;
+    private JLabel resultLabel;
+
+    public NinteenSpFourA() {
+        setTitle("Circle Area Calculator");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(3, 1));
+
+        radiusTextField = new JTextField();
+        JLabel radiusLabel = new JLabel("Enter Radius:");
+        resultLabel = new JLabel("Area: ");
+
+        JButton calculateButton = new JButton("Calculate");
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculateArea();
+            }
+        });
+
+        panel.add(radiusLabel);
+        panel.add(radiusTextField);
+        panel.add(calculateButton);
+        panel.add(resultLabel);
+
+        add(panel);
+    }
+
+    private void calculateArea() {
+        try {
+            double radius = Double.parseDouble(radiusTextField.getText());
+            double area = Math.PI * radius * radius;
+            resultLabel.setText("Area: " + area);
+        } catch (NumberFormatException ex) {
+            resultLabel.setText("Invalid input");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new NinteenSpFourA().setVisible(true));
+    }
+}
